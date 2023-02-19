eabea9857dd2c5ceb75520c39d2c924abd473121
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-02-19 23:58:28 +0200

    Added Login page HTML,CSS and logic with Validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './layouts/login/login.component';
 import { ProfileComponent } from './layouts/profile/profile.component';
 import { RegisterComponent } from './layouts/register/register.component';
 
 const routes: Routes = [
   { path:'profile', component:ProfileComponent },
   { path:'register', component:RegisterComponent },
+  { path:'login', component:LoginComponent },
 ];
 
 @NgModule({
