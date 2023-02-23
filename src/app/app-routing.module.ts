e8ce48d2c0f6cbde67ed0d2893d4b96bd93775c4
amrkhaled99 <amrkh5255@gmail.com>
2023-02-23 17:56:34 +0200

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutpageComponent } from './layouts/aboutpage/aboutpage.component';
 import { BalancepageComponent } from './layouts/balancepage/balancepage.component';
-import { LoginComponent } from './layouts/login/login.component';
 import { PaymentpageComponent } from './layouts/paymentpage/paymentpage.component';
+import { AddAssetsComponent } from './layouts/add-assets/add-assets.component';
+import { AssetsComponent } from './layouts/myassets/assets.component';
+import { MytransactionComponent } from './layouts/mytransactions/mytransaction.component';
+import { LoginComponent } from './layouts/login/login.component';
 import { ProfileComponent } from './layouts/profile/profile.component';
 import { RegisterComponent } from './layouts/register/register.component';
 
@@ -14,11 +17,13 @@ const routes: Routes = [
   { path:'balance', component:BalancepageComponent },
   { path:'payment', component:PaymentpageComponent },
   { path:'about', component:AboutpageComponent },
-
+  { path: 'assets', component: AssetsComponent },
+  { path: 'assets/add-asset', component: AddAssetsComponent },
+  { path: 'transactions', component: MytransactionComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { HomepageComponent } from './layouts/homepage/homepage.component';
+import { AssetsComponent } from './layouts/myassets/assets.component';
+import { AddAssetsComponent } from './layouts/add-assets/add-assets.component';
+import { MytransactionComponent } from './layouts/mytransactions/mytransaction.component';
 import { ProfileComponent } from './layouts/profile/profile.component';
 import { LoginComponent } from './layouts/login/login.component';
 import { RegisterComponent } from './layouts/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AboutpageComponent } from "./layouts/aboutpage/aboutpage.component";
 import { BalancepageComponent } from './layouts/balancepage/balancepage.component';
 import { PaymentpageComponent } from './layouts/paymentpage/paymentpage.component';
@@ -23,7 +25,13 @@ import { PaymentpageComponent } from './layouts/paymentpage/paymentpage.componen
     AboutpageComponent,
     BalancepageComponent,
     RegisterComponent,
-    PaymentpageComponent
+    PaymentpageComponent,
+    AssetsComponent,
+    AddAssetsComponent,
+    MytransactionComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +40,6 @@ import { PaymentpageComponent } from './layouts/paymentpage/paymentpage.componen
     ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
