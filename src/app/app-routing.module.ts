ff57970bee8e9ae3fda9f2e74b3b7079ab4e75e9
AliHussein <alihusseiinali@gmail.com>
2023-02-23 16:39:47 +0200

    my transactions and assets pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddAssetsComponent } from './layouts/add-assets/add-assets.component';
+import { AssetsComponent } from './layouts/myassets/assets.component';
+import { MytransactionComponent } from './layouts/mytransactions/mytransaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AssetsComponent },
+  { path: 'assets', component: AssetsComponent },
+  { path: 'add-asset', component: AddAssetsComponent },
+  { path: 'transactions', component: MytransactionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './layouts/homepage/homepage.component';
+import { AssetsComponent } from './layouts/myassets/assets.component';
+import { AddAssetsComponent } from './layouts/add-assets/add-assets.component';
+import { MytransactionComponent } from './layouts/mytransactions/mytransaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AssetsComponent,
+    AddAssetsComponent,
+    MytransactionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
