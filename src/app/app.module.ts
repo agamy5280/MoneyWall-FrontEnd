101749e95554472a8486d38b29a03994ec90a96e
MustafaAdel <mustafaadelsoftwaredeveloper@gmail.com>
2023-02-24 22:49:44 +0200

    create a page for make a transction 'sending money'  from user to another

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './layouts/homepage/homepage.component';
+import { HomepageComponent } from './layout/components/homepage/homepage.component';
+import { MoneyTransferComponent } from './layout/components/money-transfer/money-transfer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MoneyTransferComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
