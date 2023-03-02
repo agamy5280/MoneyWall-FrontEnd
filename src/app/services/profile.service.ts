9ad49b540f574e66b7bb665a71711263b4872533
amrkhaled99 <amrkh5255@gmail.com>
2023-03-02 17:01:35 +0200

    fetch user data

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUserdata(): any {
+    // return this.httpClient.get(`${environment.apiUrl}categories/`);
+    return this.httpClient.get('http://localhost:8000/api/profile/');
+
+  }
+}
