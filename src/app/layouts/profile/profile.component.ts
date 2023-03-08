b73bcce5bd8edaaa48295b8ddd1d867d5ce368f1
amrkhaled99 <amrkh5255@gmail.com>
2023-03-08 21:30:23 +0200

    profile V2

--- src/app/layouts/profile/profile.component.ts
+++ src/app/layouts/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'src/app/services/user.service';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { User } from 'src/app/Interfaces/user';
 import { ProfileService } from 'src/app/services/profile_api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -14,32 +15,37 @@ export class ProfileComponent implements OnInit {
   userId:string="";
   errorMsg: string = '';
   SuccessMsg:string=""
+  nonWhitespaceRegExp: RegExp = new RegExp("\\S");
+
 
   
   // public user: User[] =[];
-  constructor(private userService: UserService,private profileService:ProfileService) {
+  constructor(private userService: UserService,private profileService:ProfileService,private _router: Router) {
 
   }
   profileForm = new FormGroup({
-    firstName: new FormControl(this.user.fname, [Validators.pattern('[A-Za-z]+')]),
+    userName: new FormControl("", [Validators.maxLength(22)]),
+    firstName: new FormControl('', [Validators.pattern('[A-Za-z]+')]),
     lastName: new FormControl('', [Validators.pattern('[A-Za-z]+')]),
     birthday: new FormControl(''),
     mobileNum: new FormControl('', [Validators.pattern('[0-9]+'), Validators.maxLength(20), Validators.minLength(10)]),
     city: new FormControl('', [Validators.pattern('[A-Za-z]+')]),
     state: new FormControl('', [Validators.pattern('[A-Za-z]+')]),
     address: new FormControl('', [Validators.minLength(5)]),
-    zip: new FormControl('', [Validators.pattern('[0-9]+'), Validators.maxLength(8)]),
-    
+    zip: new FormControl('', [Validators.pattern('[0-9]+'), Validators.maxLength(8)]),  
+  })
 
-    
+  
+  settingForm = new FormGroup({
+    email: new FormControl('',[Validators.email,Validators.required, Validators.pattern(this.nonWhitespaceRegExp)]),
+    password: new FormControl(),
   })
 
 
-
   changePasswordForm = new FormGroup({
-    oldPassword: new FormControl('', [Validators.minLength(8)]),
-    newPassword: new FormControl('', [Validators.minLength(8)]),
-    confirmPassword: new FormControl('', [Validators.minLength(8)])
+    oldPassword: new FormControl('', [Validators.minLength(2)]),
+    newPassword: new FormControl('', [Validators.minLength(2)]),
+    confirmPassword: new FormControl('', [Validators.minLength(2)])
   });
   passwordMatchValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
@@ -53,12 +59,36 @@ export class ProfileComponent implements OnInit {
       }
     };
   }
+
+    
   ngOnInit() {
+    this.user=  this.userService.getUserData();
+    this.userId=this.userService.getUserID();
+
+
+
+    this.profileForm.setValue(
+     {
+       userName: this.user.username,
+       firstName:this.user.fname,
+       lastName: this.user.lname,
+       birthday: this.user.birthday,
+       mobileNum:this.user.mobile,
+       city: this.user.city,
+       state: this.user.country,
+       address: this.user.address,
+       zip: this.user.zip
+     });
+
+
+   
+   
+ 
+
     this.changePasswordForm.get('confirmPassword')?.setValidators(this.passwordMatchValidator());
 
     
-     this.user=  this.userService.getUserData();
-      this.userId=this.userService.getUserID();
+     
 
 
 console.log(this.userService.getData().access_token)
@@ -69,22 +99,32 @@ console.log(this.userService.getData().access_token)
   // editProfile() {
   //   console.log(this.profileForm.value)
   // }
-  changePassword() {
-    console.log(this.changePasswordForm.value)
+  async changePassword() {
+    (await this.profileService.changeProfilePasswordRequest(this.changePasswordForm.value,this.userId)).subscribe({
+      next: (res:any) => console.log(res),
+      error: (err:any) =>  {alert("Your password does not match any account . Please try again")},
+      complete: () => {alert("Your profile has been updated successfully !"), this._router.navigate(['home']);}
+
+    })  
   }
 
 
   async editProfile() {
-  //   (await this.profileService.UpdateProfileRequest(this.profileForm.value,this.userId)).subscribe({
-  //     next: (res:any) => console.log(res),
-  //     error: (err:any) => this.errorMsg = err.error,
-  //     complete: () => {alert("You have successfully updated your profile");}
-  //   })
+    (await this.profileService.updateProfileRequest(this.profileForm.value,this.userId)).subscribe({
+      next: (res:any) => console.log(res),
+      error: (err:any) => this.errorMsg = err.error,
+      complete: () => {alert("Your profile has been updated successfully !"), this._router.navigate(['home']);}
 
-  (await this.profileService.updateProfileRequest(this.profileForm.value,this.userId)).subscribe({})
-  }
+    })}
+
 
   
+  async deleteAcount() {
+    (await this.profileService.deleteAccountRequest(this.settingForm.value,this.userId)).subscribe({
+      next: (res:any) => console.log(res),
+      error: (err:any) =>  {alert("Your email and your password does not match . Please try again")},
+      complete: () => {alert("Your account deleted successfully"), this._router.navigate(['login']);}
 
+    })}
 
 }
--- src/app/services/profile_api.service.ts
+++ src/app/services/profile_api.service.ts
@@ -4,7 +4,6 @@ import { environment } from 'src/environments/environments';
 import { UserService } from 'src/app/services/user.service';
 
 
-import { User } from '../Interfaces/user';
 
 
 @Injectable({
@@ -12,36 +11,53 @@ import { User } from '../Interfaces/user';
 })
 export class ProfileService {
     user:UserService=new UserService;
-    //  headers:HttpHeaders=new HttpHeaders;
   constructor(private http: HttpClient) {
-
 
    }
 
 
+     // Update Profile information
+
   async updateProfileRequest(userInfo: object,id:string) {
-    // let headers = new HttpHeaders()
-    // .set('Authorization', 'Bearer ' + this.user.user_accessToken())
-    // .set('Content-Type', 'application/json')
-    // .set('InstanceName', 'ORISSA');
-    // this.headers = new HttpHeaders().set("Authorization", 'Bearer ' + this.token);
-     console.log(this.user.user_accessToken())
-     console.log(this.user.getUserData())
 
-    
-
-      
-    const accesToken="ZShGf956xv394RhT6SLwz6nli0XetcndW9yKj6uJUVVUBHKrKu7EqRPTl4fiFyZpxVi8NidZsBw4EeSWyc4k8ulVOvJof1Qd6jKs";
-    
     let reqHeader = new HttpHeaders({ 
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' +this.user.user_accessToken()
    });
 
-  //  const reqHeader = new HttpHeaders().set('Authorization', 'Bearer ' + g);
-
-    // let head_obj=new HttpHeaders().set("Authorization","Bearer"+accesToken)
-    return this.http.patch(`${environment.apiURL}user/profile/update/${41}`, userInfo,{headers:reqHeader} );
+  
+  return this.http.patch(`${environment.apiURL}user/profile/update/${id}`, userInfo,{headers:reqHeader} );
    }
  
+   
+
+  // Change Password
+   async changeProfilePasswordRequest(userInfo: object,id:string) {
+
+    let reqHeader = new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' +this.user.user_accessToken()
+   });
+    return this.http.patch(`${environment.apiURL}user/profile/changePassword/${id}`, userInfo,{headers:reqHeader} );
+   }
+ 
+
+
+   async deleteAccountRequest(userInfo: object,id:string) {
+    let reqHeader = new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' +this.user.user_accessToken()
+   });
+
+
+   const data = {
+    headers: reqHeader,
+    body: JSON.stringify(userInfo)
+ };
+ //  return this.http.delete(`${environment.apiURL}user/profile/deleteAccount/${id}`,{headers:reqHeader}  );
+
+  return this.http.delete(`${environment.apiURL}user/profile/deleteAccount/${id}`, data );
+   }
+
+
 }
